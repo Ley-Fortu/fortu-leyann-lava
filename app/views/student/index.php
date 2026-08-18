d0280b8628c676483be3bc444ebf16ada6d23a5f
Ley-Fortu <leyannfortu@gmail.com>
2026-08-18 22:34:37 +0800

    Initial Lavalust project

---
+++ app/views/student/index.php
@@ -0,0 +1,4 @@
+<h1>Student Home</h1>
+<p>Welcome to the Student Information Page</p>
+<a href="<?= site_url('student/profile'); ?>">Go to Profile</a>
+
---
+++ app/views/student/profile.php
@@ -0,0 +1,8 @@
+<h1>Student Information</h1>
+<p>Student ID: <?= $student['student_id']; ?></p>
+<p>Name: <?= $student['name']; ?></p>
+<p>Course: <?= $student['course']; ?></p>
+<p>Year Level: <?= $student['year']; ?></p>
+<p>Section: <?= $student['section']; ?></p>
+<p>Email: <?= $student['email']; ?></p>
+<a href="<?= site_url('student'); ?>">Back to Home</a>
